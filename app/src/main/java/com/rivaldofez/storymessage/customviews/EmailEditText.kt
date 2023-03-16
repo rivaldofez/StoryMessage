99edcaa839a5bc3d99dffc098067296bcfdd2184
rivaldofez <rivaldofez@gmail.com>
2023-03-16 23:09:07 +0700

    implement custom email EditText

--- app/src/main/java/com/rivaldofez/storymessage/customviews/EmailEditText.kt
+++ app/src/main/java/com/rivaldofez/storymessage/customviews/EmailEditText.kt
@@ -1,4 +1,66 @@
 package com.rivaldofez.storymessage.customviews
 
-class EmailEditText {
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.rivaldofez.storymessage.R
+
+class EmailEditText: AppCompatEditText {
+
+    private lateinit var iconEmailDrawable: Drawable
+
+    constructor(context: Context) : super(context){
+        init()
+    }
+
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init()
+    }
+
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ){
+        init()
+    }
+
+    private fun init(){
+        iconEmailDrawable = ContextCompat.getDrawable(context, R.drawable.ic_email_24) as Drawable
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        compoundDrawablePadding = 16
+
+        setHint(R.string.edt_title_email)
+        setAutofillHints(AUTOFILL_HINT_EMAIL_ADDRESS)
+        setDrawable(iconEmailDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!p0.isNullOrEmpty() && !Patterns.EMAIL_ADDRESS.matcher(p0).matches()){
+                    error = "Email address is not valid"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
 }
