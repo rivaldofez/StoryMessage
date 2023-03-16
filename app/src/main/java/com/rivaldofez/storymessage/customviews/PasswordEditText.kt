d553ef15548eb734dba2efffc730b1ba9a2afb77
rivaldofez <rivaldofez@gmail.com>
2023-03-17 00:23:08 +0700

    implement password edit text

--- app/src/main/java/com/rivaldofez/storymessage/customviews/PasswordEditText.kt
+++ app/src/main/java/com/rivaldofez/storymessage/customviews/PasswordEditText.kt
@@ -1,6 +1,66 @@
 package com.rivaldofez.storymessage.customviews
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.rivaldofez.storymessage.R
 
 class PasswordEditText: AppCompatEditText {
+    private lateinit var iconPasswordDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ){
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+
+    private fun init(){
+        iconPasswordDrawable = ContextCompat.getDrawable(context, R.drawable.ic_password_24) as Drawable
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        compoundDrawablePadding = 16
+
+        setHint(R.string.edt_title_password)
+        setDrawable(iconPasswordDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!p0.isNullOrEmpty() && p0.length <= 8 ){
+                    error = "Password length minimum 8"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
 }
