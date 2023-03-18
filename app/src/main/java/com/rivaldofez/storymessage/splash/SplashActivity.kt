a78d587c453dd1bdbc93f6e5cd4380766dea45b9
rivaldofez <rivaldofez@gmail.com>
2023-03-18 22:32:46 +0700

    add splash activity

--- app/src/main/java/com/rivaldofez/storymessage/splash/SplashActivity.kt
+++ app/src/main/java/com/rivaldofez/storymessage/splash/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.rivaldofez.storymessage
+package com.rivaldofez.storymessage.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.storymessage.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
