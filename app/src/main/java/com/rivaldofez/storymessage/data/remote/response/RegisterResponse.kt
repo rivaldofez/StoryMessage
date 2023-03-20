3864778aead26b245f99880c889a403c74f96eb0
rivaldofez <rivaldofez@gmail.com>
2023-03-20 12:08:07 +0700

    implement data class register

--- app/src/main/java/com/rivaldofez/storymessage/data/remote/response/RegisterResponse.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/remote/response/RegisterResponse.kt
@@ -1,3 +1,12 @@
 package com.rivaldofez.storymessage.data.remote.response
 
-data class RegisterResponse()
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String
+)
