33619d83c567a81d3d904405be79cfb18d37a34f
rivaldofez <rivaldofez@gmail.com>
2023-03-20 12:20:57 +0700

    implement api service for login and register

--- app/src/main/java/com/rivaldofez/storymessage/data/remote/ApiService.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/remote/ApiService.kt
@@ -1,4 +1,25 @@
 package com.rivaldofez.storymessage.data.remote
 
-class ApiService {
+import com.rivaldofez.storymessage.data.remote.response.LoginResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun userLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun userRegister(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    )
+
 }
