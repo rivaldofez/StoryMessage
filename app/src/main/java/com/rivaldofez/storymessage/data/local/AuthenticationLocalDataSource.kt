ddbddfd1ea062ee14c0107055c4dd32188f12125
rivaldofez <rivaldofez@gmail.com>
2023-03-22 23:23:02 +0700

    implement read and save authentication token

--- app/src/main/java/com/rivaldofez/storymessage/data/local/AuthenticationLocalDataSource.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/local/AuthenticationLocalDataSource.kt
@@ -1,4 +1,28 @@
 package com.rivaldofez.storymessage.data.local
 
-class AuthenticationLocalDataSource {
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AuthenticationLocalDataSource constructor(private val dataStore: DataStore<Preferences>) {
+
+    fun getAuthenticationToken(): Flow<String?> {
+        return dataStore.data.map { preferences ->
+            preferences[TOKEN_KEY]
+        }
+    }
+
+    suspend fun saveAuthenticationToken(token: String){
+        dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
+
+    companion object {
+        private val TOKEN_KEY = stringPreferencesKey("token_key")
+    }
 }
