8f06c0704cf18281cd6fec986930c91ea50a7cc9
rivaldofez <rivaldofez@gmail.com>
2023-03-23 16:28:51 +0700

    change splash activity to splash fragment

---
+++ app/src/main/java/com/rivaldofez/storymessage/splash/SplashFragment.kt
@@ -0,0 +1,65 @@
+package com.rivaldofez.storymessage.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.rivaldofez.storymessage.BaseActivity
+import com.rivaldofez.storymessage.R
+import com.rivaldofez.storymessage.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    private val SPLASH_TIME: Long = 3000
+    private val splashViewModel: SplashViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(requireActivity()).load(R.drawable.placeholder).into(binding.imgLogo)
+        Handler(Looper.getMainLooper()).postDelayed({
+            checkUserSession()
+        }, SPLASH_TIME)
+
+    }
+
+    private fun checkUserSession(){
+        lifecycleScope.launchWhenCreated {
+            launch {
+                splashViewModel.getAuthenticationToken().collect { token ->
+                    Log.d("Hexa", "Test print")
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
