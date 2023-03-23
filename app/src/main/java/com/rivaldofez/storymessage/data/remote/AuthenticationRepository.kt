86eabb9115b70a3eccb66fd067fe95a8bc4902ae
rivaldofez <rivaldofez@gmail.com>
2023-03-23 12:02:07 +0700

    implement AuthenticationRepository for user login

--- app/src/main/java/com/rivaldofez/storymessage/data/remote/AuthenticationRepository.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/remote/AuthenticationRepository.kt
@@ -1,4 +1,23 @@
 package com.rivaldofez.storymessage.data.remote
 
-class AuthenticationRepository {
+import com.rivaldofez.storymessage.data.local.AuthenticationLocalDataSource
+import com.rivaldofez.storymessage.data.remote.response.LoginResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class AuthenticationRepository constructor(
+    private val apiService: ApiService,
+    private val authenticationLocalDataSource: AuthenticationLocalDataSource) {
+
+
+
+    suspend fun userLogin(email: String, password: String): Flow<Result<LoginResponse>> = flow {
+        try {
+            val response = apiService.userLogin(email = email, password = password)
+            emit(Result.success(response))
+        } catch (e: Exception){
+            e.printStackTrace()
+            emit(Result.failure(e))
+        }
+    }
 }
