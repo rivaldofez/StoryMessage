748ef1c3992669e5aef36493cb040ea4764ba5b8
rivaldofez <rivaldofez@gmail.com>
2023-03-23 22:40:02 +0700

    add story adapter

---
+++ app/src/main/java/com/rivaldofez/storymessage/story/StoryAdapter.kt
@@ -0,0 +1,55 @@
+package com.rivaldofez.storymessage.story
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.rivaldofez.storymessage.data.remote.response.StoryResponse
+import com.rivaldofez.storymessage.databinding.ItemStoryBinding
+
+class StoryAdapter: ListAdapter<StoryResponse, StoryAdapter.ViewHolder>(DiffCallback) {
+
+
+    class ViewHolder(private val binding: ItemStoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(context: Context, story: StoryResponse) {
+            binding.apply {
+                tvStoryUsername.text = story.name
+                tvStoryDescription.text = story.description
+
+                // On item clicked
+
+            }
+        }
+    }
+
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<StoryResponse>() {
+            override fun areItemsTheSame(oldItem: StoryResponse, newItem: StoryResponse): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: StoryResponse, newItem: StoryResponse): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val story = getItem(position)
+        holder.bind(holder.itemView.context, story)
+    }
+
+}
