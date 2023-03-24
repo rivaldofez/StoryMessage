53fb5e142747376f58f0d6e2f9e06fb0041e28e8
rivaldofez <rivaldofez@gmail.com>
2023-03-24 12:44:01 +0700

    implement select image from camera and gallery

--- app/src/main/java/com/rivaldofez/storymessage/addstory/AddStoryFragment.kt
+++ app/src/main/java/com/rivaldofez/storymessage/addstory/AddStoryFragment.kt
@@ -1,60 +1,130 @@
 package com.rivaldofez.storymessage.addstory
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
 import com.rivaldofez.storymessage.R
+import com.rivaldofez.storymessage.Utils.MediaUtility
+import com.rivaldofez.storymessage.Utils.MediaUtility.uriToFile
+import com.rivaldofez.storymessage.databinding.FragmentAddStoryBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddStoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddStoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentAddStoryBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var currentPhotoPath: String
+    private var getFile: File? = null
+    private var token: String = ""
+
+
+    private val addStoryViewModel: AddStoryViewModel by viewModels()
+
+
+    private val launcherCameraIntent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if (result.resultCode == RESULT_OK) {
+            val file = File(currentPhotoPath).also { getFile = it }
+            val os: OutputStream
+
+            // Rotate image to correct orientation
+            val bitmap = BitmapFactory.decodeFile(getFile?.path)
+            val exif = ExifInterface(currentPhotoPath)
+            val orientation: Int = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED )
+
+            val rotatedBitmap: Bitmap = when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(bitmap, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> TransformationUtils.rotateImage(bitmap, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> TransformationUtils.rotateImage(bitmap, 270)
+                ExifInterface.ORIENTATION_NORMAL -> bitmap
+                else -> bitmap
+            }
+
+            // Convert rotated image to file
+            try {
+                os = FileOutputStream(file)
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os)
+                os.flush()
+                os.close()
+
+                getFile = file
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            binding.imgStory.setImageBitmap(rotatedBitmap)
+
         }
+    }
+
+    private val launcherGalleryIntent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            uriToFile(selectedImg, requireContext()).also { getFile = it }
+
+            binding.imgStory.setImageURI(selectedImg)
+        }
+    }
+
+    private fun startCameraIntent(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(requireActivity().packageManager)
+
+        MediaUtility.createTempFile(requireActivity()).also {
+            val photoUri =
+                FileProvider.getUriForFile(requireContext(), "com.rivaldofez.storymessage", it)
+
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            launcherCameraIntent.launch(intent)
+        }
+    }
+
+    private fun startGalleryIntent(){
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherGalleryIntent.launch(chooser)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_story, container, false)
+       _binding = FragmentAddStoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddStoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddStoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCamera.setOnClickListener { startCameraIntent() }
+        binding.btnGallery.setOnClickListener { startGalleryIntent() }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
