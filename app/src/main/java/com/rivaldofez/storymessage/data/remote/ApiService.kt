cb25cec7c8612673da6067014925ed679a49037c
rivaldofez <rivaldofez@gmail.com>
2023-03-24 11:37:01 +0700

    implement add story to repository

--- app/src/main/java/com/rivaldofez/storymessage/data/remote/ApiService.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/remote/ApiService.kt
@@ -4,6 +4,7 @@ import com.rivaldofez.storymessage.data.remote.response.AddStoryResponse
 import com.rivaldofez.storymessage.data.remote.response.LoginResponse
 import com.rivaldofez.storymessage.data.remote.response.RegisterResponse
 import com.rivaldofez.storymessage.data.remote.response.StoriesResponse
+import okhttp3.MultipartBody
 import okhttp3.MultipartReader
 import okhttp3.RequestBody
 import retrofit2.http.*
@@ -37,7 +38,7 @@ interface ApiService {
     @POST("stories")
     suspend fun addStory(
         @Header("Authorization") token: String,
-        @Part file: MultipartReader.Part,
+        @Part file: MultipartBody.Part,
         @Part("description") description: RequestBody
     ): AddStoryResponse
 
