cf522bce25b1d5d4101da8720a66577a41aba4f0
rivaldofez <rivaldofez@gmail.com>
2023-04-04 22:56:15 +0700

    add google maps fragment

--- app/src/main/java/com/rivaldofez/storymessage/page/splash/SplashFragment.kt
+++ app/src/main/java/com/rivaldofez/storymessage/page/splash/SplashFragment.kt
@@ -1,8 +1,6 @@
 package com.rivaldofez.storymessage.page.splash
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.rivaldofez.storymessage.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -33,15 +32,15 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            checkUserSession()
-        }, 3000)
+        checkUserSession()
+
     }
 
     private fun checkUserSession(){
         viewLifecycleOwner.lifecycleScope.launchWhenCreated {
             launch {
                 splashViewModel.getAuthenticationToken().collect { token ->
+                    delay(3000)
                     if (token.isNullOrEmpty()) {
                         val goToLogin =
                             SplashFragmentDirections.actionSplashFragmentToLoginFragment()
