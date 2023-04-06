e95b1e3ff456961e8eeb8ba63bc804f778a588c6
rivaldofez <rivaldofez@gmail.com>
2023-04-06 16:54:56 +0700

    implement paging in story repository

--- app/src/main/java/com/rivaldofez/storymessage/data/StoryRepository.kt
+++ app/src/main/java/com/rivaldofez/storymessage/data/StoryRepository.kt
@@ -1,6 +1,13 @@
 package com.rivaldofez.storymessage.data
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.rivaldofez.storymessage.data.local.entity.StoryEntity
+import com.rivaldofez.storymessage.data.local.room.StoryDatabase
 import com.rivaldofez.storymessage.data.remote.ApiService
+import com.rivaldofez.storymessage.data.remote.StoryRemoteMediator
 import com.rivaldofez.storymessage.data.remote.response.AddStoryResponse
 import com.rivaldofez.storymessage.data.remote.response.StoriesResponse
 import kotlinx.coroutines.Dispatchers
@@ -11,20 +18,31 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import javax.inject.Inject
 
+@ExperimentalPagingApi
 class StoryRepository @Inject constructor(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val storyDatabase: StoryDatabase
 ) {
 
-    suspend fun getStories(token: String, page: Int?, size: Int?): Flow<Result<StoriesResponse>> = flow{
-        try {
-            val bearerToken = "Bearer $token"
-            val response = apiService.getStories(token = bearerToken, page = page, size = size)
-            emit(Result.success(response))
-        } catch (e: Exception){
-            e.printStackTrace()
-            emit(Result.failure(e))
-        }
-    }.flowOn(Dispatchers.IO)
+//    suspend fun getStories(token: String, page: Int?, size: Int?): Flow<Result<StoriesResponse>> = flow{
+//        try {
+//            val bearerToken = "Bearer $token"
+//            val response = apiService.getStories(token = bearerToken, page = page, size = size)
+//            emit(Result.success(response))
+//        } catch (e: Exception){
+//            e.printStackTrace()
+//            emit(Result.failure(e))
+//        }
+//    }.flowOn(Dispatchers.IO)
+
+    fun getStories(token: String): Flow<PagingData<StoryEntity>> {
+        val bearerToken = "Bearer $token"
+        return Pager(
+            config = PagingConfig(pageSize = 15),
+            remoteMediator = StoryRemoteMediator(storyDatabase, apiService, bearerToken),
+            pagingSourceFactory = { storyDatabase.storyDao().getAllStories() }
+        ).flow
+    }
 
 
     suspend fun addStory(token: String, file: MultipartBody.Part, description: RequestBody)
