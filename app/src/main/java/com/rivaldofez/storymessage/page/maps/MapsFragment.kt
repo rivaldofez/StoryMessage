8caa3faa1ab79825c80a34cf53818e45febd5b2c
rivaldofez <rivaldofez@gmail.com>
2023-04-07 01:22:49 +0700

    implement maps feature

--- app/src/main/java/com/rivaldofez/storymessage/page/maps/MapsFragment.kt
+++ app/src/main/java/com/rivaldofez/storymessage/page/maps/MapsFragment.kt
@@ -1,11 +1,21 @@
 package com.rivaldofez.storymessage.page.maps
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.ExperimentalPagingApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,35 +24,112 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.rivaldofez.storymessage.R
+import com.rivaldofez.storymessage.databinding.FragmentMapsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@ExperimentalPagingApi
+@AndroidEntryPoint
 class MapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var vMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var token: String = ""
+    private val mapsViewModel: MapsViewModel by viewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                getCurrentLocation()
+            }
+        }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        vMap = googleMap
+        vMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        getCurrentLocation()
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            mapsViewModel.getAuthenticationToken().collect { token ->
+                if (!token.isNullOrEmpty()){
+                    this@MapsFragment.token = token
+
+                    fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+                    setStoryMarker()
+                }
+            }
+        }
+    }
+
+    private fun getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext().applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            vMap.isMyLocationEnabled = true
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    vMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 8f))
+                } else {
+                    Toast.makeText(requireContext(), "Aktifkan Lokasi", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    private fun setStoryMarker() {
+        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
+            launch {
+                mapsViewModel.getStories(token).collect { result ->
+                    result.onSuccess { response ->
+                        response.stories.forEach { story ->
+
+                            // Verify that latitude and longitude field not null
+                            // Create marker on the map
+                            if (story.lat != null && story.lon != null) {
+                                val latLng = LatLng(story.lat, story.lon)
+
+                                vMap.addMarker(
+                                    MarkerOptions()
+                                        .position(latLng)
+                                        .title(story.name)
+                                        .snippet("Lat: ${story.lat}, Lon: ${story.lon}")
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/rivaldofez/storymessage/page/maps/MapsViewModel.kt
+++ app/src/main/java/com/rivaldofez/storymessage/page/maps/MapsViewModel.kt
@@ -2,6 +2,7 @@ package com.rivaldofez.storymessage.page.maps
 
 import androidx.lifecycle.ViewModel
 import androidx.paging.ExperimentalPagingApi
+import com.rivaldofez.storymessage.data.AuthenticationRepository
 import com.rivaldofez.storymessage.data.StoryRepository
 import com.rivaldofez.storymessage.data.remote.response.StoriesResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,9 +11,13 @@ import javax.inject.Inject
 
 @ExperimentalPagingApi
 @HiltViewModel
-class MapsViewModel @Inject constructor(private val storyRepository: StoryRepository) :
+class MapsViewModel @Inject constructor(
+    private val authenticationRepository: AuthenticationRepository,
+    private val storyRepository: StoryRepository) :
     ViewModel() {
 
-    fun getAllStories(token: String): Flow<Result<StoriesResponse>> =
+    fun getStories(token: String): Flow<Result<StoriesResponse>> =
         storyRepository.getStoriesWithLocation(token)
+
+    fun getAuthenticationToken(): Flow<String?> = authenticationRepository.getAuthenticationToken()
 }
