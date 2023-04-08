b785ecb8647793be6e124210e125f15a552edf18
rivaldofez <rivaldofez@gmail.com>
2023-04-09 00:40:29 +0700

    implement story view model test

---
+++ app/src/test/java/com/rivaldofez/storymessage/page/story/StoryViewModelTest.kt
@@ -0,0 +1,73 @@
+package com.rivaldofez.storymessage.page.story
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.AsyncPagingDataDiffer
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.ListUpdateCallback
+import com.rivaldofez.storymessage.data.local.entity.StoryEntity
+import com.rivaldofez.storymessage.util.DataDummy
+import com.rivaldofez.storymessage.utils.CoroutineTestRule
+import com.rivaldofez.storymessage.utils.PagedTestDataSource
+import com.rivaldofez.storymessage.utils.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@ExperimentalPagingApi
+@RunWith(MockitoJUnitRunner::class)
+class StoryViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
+    @Mock
+    private lateinit var storyViewModel: StoryViewModel
+
+    private val dummyToken = "authentication_token"
+
+    @Test
+    fun `Get all stories successfully`() = runTest {
+        val dummyStories = DataDummy.generateDummyListStory()
+        val data = PagedTestDataSource.snapshot(dummyStories)
+
+        val stories = MutableLiveData<PagingData<StoryEntity>>()
+        stories.value = data
+
+        Mockito.`when`(storyViewModel.getStories(dummyToken)).thenReturn(stories)
+
+        val actualStories = storyViewModel.getStories(dummyToken).getOrAwaitValue()
+        val differ = AsyncPagingDataDiffer(
+            diffCallback = StoryAdapter.DiffCallback,
+            updateCallback = noopListUpdateCallback,
+            mainDispatcher = coroutinesTestRule.testDispatcher,
+            workerDispatcher = coroutinesTestRule.testDispatcher
+        )
+        differ.submitData(actualStories)
+
+        advanceUntilIdle()
+
+        Mockito.verify(storyViewModel).getStories(dummyToken)
+        assertNotNull(differ.snapshot())
+        assertEquals(dummyStories.size, differ.snapshot().size)
+    }
+
+
+    private val noopListUpdateCallback = object : ListUpdateCallback {
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+        override fun onMoved(fromPosition: Int, toPosition: Int) {}
+        override fun onChanged(position: Int, count: Int, payload: Any?) {}
+    }
+}
